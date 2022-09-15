b4c9df03e0067d13907ed9887133f2454592d8a2
zaidkhan9085 <100270238+zaidkhan9085@users.noreply.github.com>
2022-09-15 20:21:54 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+//Author : Zaid khan java-C2533
+//Date : 15 / 8/ 22
+class Pattern
+{
+public static void main(String [] args)
+{
+for(int i =1 ; i<=5; i++)
+{
+  for(int j=5 ; j>=i; j--)
+  {
+  System.out.print("*");
+  }
+  System.out.println("");
+}
+}
+}
